68303d45c22c602cb1e728ea8ec2a4595a63ad91
Mughees <smughees78@gmail.com>
2026-02-12 19:43:35 +0500

    update .env

--- models/index.js
+++ models/index.js
@@ -1,20 +1,3 @@
-// const sequelize = require('../config/db');
-// const User = require('./user.model')
-
-
-// // sync all models
-// async function syncAll() {
-//     try{
-//         await sequelize.sync();
-//         console.log('All tables synced with relationships');
-//     }catch (error){
-//         console.error('Sync error:', error.message)
-//     }
-// }
-
-// module.exports = { User }
-
-
 const { DataTypes } = require('sequelize')
 const sequelize = require('../config/db')
 const Student = require('./student')(sequelize, DataTypes)
